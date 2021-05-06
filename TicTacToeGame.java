fbfa80201c05cb4dc803f0cb25f6bc25c6c6abd4
prudhvi <prudhvieethakota123@gmail.com>
2021-05-06 08:53:41 +0530

    Added:Code for Initializing Board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,11 +1,35 @@
 package tictactoe;
 
+/**
+ * @description create Class for defining the TicTacToeGame
+ * @param None
+ * 
+ */
 public class TicTacToeGame {
+	static String[] board;
 
+	/**
+	 * @description Private Method to initializing TicTacToeBoard and set the board
+	 *              to all empty values
+	 * @param None
+	 * 
+	 */
+	private void IntializingBoard() {
+		for (int a = 0; a < 9; a++) { // for loop to initializing board
+			board[a] = String.valueOf(a + 1); //values stores in array string
+		}
+	}
+
+	/**
+	 * @description Method to Create Current Board
+	 * @param None
+	 * 
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Tic-Tac-Toe Game");
-
+		TicTacToeGame tictactoegame = new TicTacToeGame();// Create Object related to Class
+		tictactoegame.IntializingBoard();// relation between Main and InitializingBoard Method
 	}
 
 }
